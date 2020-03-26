ba5e1c9089b18b1fd99bf5a82f3458a203e16433
Robson Maia <robsonmvieira@gmail.com>
2020-03-25 21:44:20 -0300

    feat: add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'sobre', component: AboutComponent
+  },
+  {
+    path: 'contato', component: ContactComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(p => p.PostsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
