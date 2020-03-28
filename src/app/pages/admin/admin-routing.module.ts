b86c731fa34688bdc8a43b203920b17506fe3e44
Robson Maia <robsonmvieira@gmail.com>
2020-03-28 17:09:42 -0300

    feat: add categoryList in admin module

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryListComponent } from '../category/category-list/category-list.component';
 
 const routes: Routes = [
   {
     path: '', component: DashboardComponent
   }
-]
+];
 
 @NgModule({
   imports: [
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminRouterModule } from './admin-routing.module';
 import { EditorModule } from '@tinymce/tinymce-angular';
+import { AdminComponent } from './admin.component';
+import { CategoryListComponent } from '../category/category-list/category-list.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, AdminComponent, CategoryListComponent],
   imports: [
     CommonModule,
     AdminRouterModule,
