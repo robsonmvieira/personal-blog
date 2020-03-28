81e27e24b8a34e46cd107ca6d7831e6c345945ca
Robson Maia <robsonmvieira@gmail.com>
2020-03-28 17:04:01 -0300

    feat: create a resource with category object mock

---
+++ src/app/helpers/resources/category-item.ts
@@ -0,0 +1,27 @@
+export const categories = [
+  {
+    id: 1,
+    title: 'Categories',
+    svg: '../../../assets/img/admin-img/list.svg'
+  },
+  {
+    id: 2,
+    title: 'Tags',
+    svg: '../../../assets/img/admin-img/discount.svg'
+  },
+  {
+    id: 3,
+    title: 'Posts',
+    svg: '../../../assets/img/admin-img/discount.svg'
+  },
+  {
+    id: 4,
+    title: 'Comments',
+    svg: '../../../assets/img/admin-img/comment.svg'
+  },
+  {
+    id: 5,
+    title: 'Users',
+    svg: '../../../assets/img/admin-img/support.svg'
+  },
+]
