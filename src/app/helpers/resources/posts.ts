652c42f08c0521ea1c336a5f54ca4a23c6397643
Robson Maia <robsonmvieira@gmail.com>
2020-03-29 13:18:45 -0300

    chore: add mocks post

---
+++ src/app/helpers/resources/posts.ts
@@ -0,0 +1,52 @@
+export const posts = [
+  {
+    id: 1,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam perferendis atque minima at! Explicabo, quae.',
+    comments: []
+  },
+  {
+    id: 2,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing e',
+    comments: []
+  },
+  {
+    id: 3,
+    title: '  Lorem ipsum dolor sit amet consectetur adipisicing elit.Qui, expedita minima.',
+    comments: []
+  },
+  {
+    id: 4,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam perferendis atque minima at! Explicabo, quae.',
+    comments: []
+  },
+  {
+    id: 5,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam perferendis atque minima at! Explicabo, quae.',
+    comments: []
+  },
+  {
+    id: 6,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam perferendis atque minima at! Explicabo, quae.',
+    comments: []
+  },
+  {
+    id: 7,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam perferendis atque minima at! Explicabo, quae.',
+    comments: []
+  },
+  {
+    id: 8,
+    title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam perferendis atque minima at! Explicabo, quae.',
+    comments: []
+  },
+  {
+    id: 9,
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, ipsum?',
+    comments: []
+  },
+  {
+    id: 10,
+    title: ' Lorem ipsum dolor sit amet.',
+    comments: []
+  }
+]
