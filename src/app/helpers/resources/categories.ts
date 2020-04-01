f406b4903dfa686c28032f74646d91d4754f8cee
Robson Maia <robsonmvieira@gmail.com>
2020-04-01 20:51:12 -0300

    feat: add categoryList mock data

---
+++ src/app/helpers/resources/categories.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    id:0,
+    title: 'Front-end'
+  },
+  {
+    id:1,
+    title: 'Backend-end'
+  },
+  {
+    id:2,
+    title: 'Design Pattern'
+  },
+  {
+    id:3,
+    title: 'Banco de dados'
+  },
+  {
+    id:4,
+    title: 'Tools'
+  }
+]
