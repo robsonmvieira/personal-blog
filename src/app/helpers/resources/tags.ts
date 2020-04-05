c8e70204d0cc6ee3b81c0a92db9f42e9ea9ac9b3
Robson Maia <robsonmvieira@gmail.com>
2020-04-05 20:24:09 -0300

    feat: create a tag mock data

---
+++ src/app/helpers/resources/tags.ts
@@ -0,0 +1,38 @@
+export const tags =[
+  {
+    id: 0,
+    title: 'front-end',
+  },
+  {
+    id: 1,
+    title: 'back-end',
+  },
+  {
+    id: 2,
+    title: 'design pattern',
+  },
+  {
+    id: 3,
+    title: 'challenges',
+  },
+  {
+    id: 4,
+    title: 'jobs',
+  },
+  {
+    id: 5,
+    title: 'studing',
+  },
+  {
+    id: 6,
+    title: 'front-end',
+  },
+  {
+    id: 7,
+    title: 'tips',
+  },
+  {
+    id: 8,
+    title: 'share',
+  }
+]
