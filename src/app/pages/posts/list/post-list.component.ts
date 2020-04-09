a35c56171ea3a8dcb9cc9b36c93904d5a33d7695
Robson Maia <robsonmvieira@gmail.com>
2020-04-09 13:44:20 -0300

    feat: add methods to show/hide add form post

--- src/app/pages/posts/list/post-list.component.ts
+++ src/app/pages/posts/list/post-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { posts } from '../../../helpers/resources/posts'
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
-
+  posts = posts
+  addNewPost = false;
   constructor() { }
 
   ngOnInit() {
+
+  }
+  showModalNewPost(){
+    this.addNewPost = !this.addNewPost;
   }
 
 }
