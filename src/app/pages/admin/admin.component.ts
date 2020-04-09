c7f85908bedc44b70414fe851d660c38b289fca4
Robson Maia <robsonmvieira@gmail.com>
2020-04-09 13:37:06 -0300

    feat:  show a selected option from menu

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component'
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
 
-export class AdminComponent {
+export class AdminComponent implements OnInit {
   isOn = false;
-  constructor() {
+  selected: string = 'Categories'
+  constructor(private dash: DashboardComponent) {
 
+  }
+
+  ngOnInit() {
+    this.dash.itemMenuSelected.subscribe( (e) => {
+      this.selected = e;
+    })
   }
 }
