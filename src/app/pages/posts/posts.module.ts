007c4790d3a8cc010e7152b8188d7729896b50b7
Robson Maia <robsonmvieira@gmail.com>
2020-04-09 13:42:51 -0300

    feat: share component for post

--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostListComponent } from './list/post-list.component';
-import { AddComponent } from './add/add.component';
+import { AddPostComponent } from './add/add.component';
 import { RemoveComponent } from './remove/remove.component';
 import { OneComponent } from './one/one.component';
 import { PostingRouterModule } from './posts-routing.module';
@@ -9,8 +9,8 @@ import { PostingRouterModule } from './posts-routing.module';
 
 
 @NgModule({
-  declarations: [PostListComponent, AddComponent, RemoveComponent, OneComponent],
-  exports: [PostListComponent, OneComponent],
+  declarations: [PostListComponent, AddPostComponent, RemoveComponent, OneComponent],
+  exports: [PostListComponent, OneComponent, AddPostComponent],
   imports: [
     CommonModule,
     PostingRouterModule
