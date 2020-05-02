c3e7552368487b67e7181b1a04f51576c681168e
Robson Maia <robsonmvieira@gmail.com>
2020-05-02 00:35:32 -0300

    feat: add comments mock data resource

---
+++ src/app/helpers/resources/comments.ts
@@ -0,0 +1,26 @@
+export const comments = [
+  {
+    id: 0,
+    post_id: 1,
+    user_id: 5,
+    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing e Lorem ipsum dolor sit, amet consectetur adipisicing e',
+  },
+  {
+    id: 1,
+    post_id: 11,
+    user_id: 9,
+    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing e Lorem ipsum dolor sit, amet consectetur adipisicing e',
+  },
+  {
+    id: 2,
+    post_id: 41,
+    user_id: 35,
+    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing e Lorem ipsum dolor sit, amet consectetur adipisicing e',
+  },
+  {
+    id: 0,
+    post_id: 1,
+    user_id: 5,
+    content: 'Lorem ipsum dolor sit, amet consectetur adipisicing e Lorem ipsum dolor sit, amet consectetur adipisicing e',
+  }
+]
