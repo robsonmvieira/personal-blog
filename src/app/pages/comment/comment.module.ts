45f944c0312838b13f9611af6cff8ad9f789ee67
Robson Maia <robsonmvieira@gmail.com>
2020-05-02 00:39:28 -0300

    feat: add and export comment components

--- src/app/pages/comment/comment.module.ts
+++ src/app/pages/comment/comment.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentListComponent } from './comment-list/comment-list.component';
 import { CommentAddComponent } from './comment-add/comment-add.component';
-
+import CommentRouterModule from './comment-routing.module';
 
 
 @NgModule({
   declarations: [CommentListComponent, CommentAddComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    CommentRouterModule
+  ],
+  exports: [CommentListComponent, CommentAddComponent]
 })
 export class CommentModule { }
