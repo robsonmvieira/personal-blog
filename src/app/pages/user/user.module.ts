8dfa95ee5f015abd3db7471990e31ff4acdd9164
Robson Maia <robsonmvieira@gmail.com>
2020-05-01 23:46:03 -0300

    feat: create user module

---
+++ src/app/pages/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserAddComponent } from './user-add/user-add.component';
+
+
+
+@NgModule({
+  declarations: [UserListComponent, UserAddComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
